d1d6194a535bd4017d2dd31a39d00e2bd9a2a595
NzmNzmi <165229892+NzmNzmi@users.noreply.github.com>
2024-04-24 23:13:58 -0500

    Ej2

---
+++ Ordenar_5_num.js
@@ -0,0 +1,8 @@
+//Ordene 5 números de menor a mayor
+let n1 = parseFloat(prompt("Escribe el primer número:"));
+let n2 = parseFloat(prompt("Escribe el segundo número:"));
+let n3 = parseFloat(prompt("Escribe el tercer número:"));
+let n4 = parseFloat(prompt("Escribe el cuarto número:"));
+let n5 = parseFloat(prompt("Escribe el quinto número:"));
+
+//nums.sort((a,b) => a - b); // [1, 2, 3, 4, 6, 10, 19]
