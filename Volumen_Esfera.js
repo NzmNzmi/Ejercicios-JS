779e4ceeca06ce8260064fb3f0c218b148018d81
NzmNzmi <165229892+NzmNzmi@users.noreply.github.com>
2024-04-24 22:51:34 -0500

    Ya salió!

--- Volumen_Esfera.js
+++ Volumen_Esfera.js
@@ -1,10 +1,5 @@
 // V = 4/3 π r³
-
-println("Vamos a calcular el volumen de una esfera!");
-
-var radio = readLine("Cuál es el radio de la esfera?");
-
-let volumen;
-volumen = 4/3 * Math.PI * radio * radio * radio;
-
-println("El valor del volumen es" + volumen);
+let radio = parseFloat(prompt("Escribe el radio:"))
+let pi = 3.1416;
+let vol = 4 / 3 * pi * ( radio * radio * radio);
+prompt ("El volumen es " + vol);
