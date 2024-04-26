c34266ade6dbd55372799aac6f31a84b1bc90ab7
NzmNzmi <165229892+NzmNzmi@users.noreply.github.com>
2024-04-25 20:18:45 -0500

    ok

--- Ordenar_5_num.js
+++ Ordenar_5_num.js
@@ -5,4 +5,10 @@ let n3 = parseFloat(prompt("Escribe el tercer número:"));
 let n4 = parseFloat(prompt("Escribe el cuarto número:"));
 let n5 = parseFloat(prompt("Escribe el quinto número:"));
 
+let numeros = [n1,n2,n3,n4,n5];
+
+numeros.sort(function(a, b){return a - b});
+
+alert("El orden es" + numeros);
+
 //nums.sort((a,b) => a - b); // [1, 2, 3, 4, 6, 10, 19]
--- Volumen_Esfera.js
+++ Volumen_Esfera.js
@@ -2,4 +2,4 @@
 let radio = parseFloat(prompt("Escribe el radio:"))
 let pi = 3.1416;
 let vol = 4 / 3 * pi * ( radio * radio * radio);
-prompt ("El volumen es " + vol);
+alert ("El volumen es " + vol);
